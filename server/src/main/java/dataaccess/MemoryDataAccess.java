104c0dfb77ac6e2b296d8e69e91c736fa27edbbc
carson <carsonbascom@gmail.com>
2026-05-26 21:36:13 -0600

    phase 3: MemoryDataAccess.java

---
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -0,0 +1,74 @@
+package dataaccess;
+import chess.ChessGame;
+import model.*;
+import java.util.*;
+
+public class MemoryDataAccess implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private int nextGameID = 1;
+
+    @Override
+    public void clear() {
+        users.clear();
+        games.clear();
+        auths.clear();
+        nextGameID = 1;
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new AlreadyTakenException("Error: already taken");
+        }
+        users.put(user.username(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public int createGame(String gameName) {
+        int id = nextGameID++;
+        games.put(id, new GameData(id, null, null, gameName, new ChessGame()));
+        return id;
+    }
+
+    @Override
+    public GameData getGame(int gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public Collection<GameData> listGames() {
+        return games.values();
+    }
+
+    @Override
+    public void updateGame(GameData game) throws DataAccessException {
+        if (!games.containsKey(game.gameID())) {
+            throw new DataAccessException("Error: game not found");
+        }
+        games.put(game.gameID(), game);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) {
+        auths.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) {
+        return auths.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (auths.remove(authToken) == null) {
+            throw new UnauthorizedException("Error: unauthorized");
+        }
+    }
+}
