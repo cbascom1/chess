ace504bf0fc84574cf8e5a71691a58a0d8b191f7
carson <carsonbascom@gmail.com>
2026-06-03 19:24:05 -0600

    phase 5: PreloginClient.java

--- client/src/main/java/ui/PreloginClient.java
+++ client/src/main/java/ui/PreloginClient.java
@@ -1,19 +1,29 @@
 package ui;
 
+import client.ResponseException;
 import client.ServerFacade;
 import java.util.Arrays;
+import client.ServerTypes.LoginResult;
+import client.ServerTypes.RegisterResult;
+
 
 
 public class PreloginClient {
     private final ServerFacade facade;
+
+    private String authToken;
+
+    public String getAuthToken() {
+        return authToken;
+    }
 
     public PreloginClient(String serverUrl) {
         this.facade = new ServerFacade(serverUrl);
     }
 
     public String eval(String input) {
-        var tokens = input.toLowerCase().split(" ");
-        var cmd = (tokens.length > 0) ? tokens[0] : "help";
+        var tokens = input.trim().split("\\s+");
+        var cmd = (tokens.length > 0) ? tokens[0].toLowerCase() : "help";
         var params = Arrays.copyOfRange(tokens, 1, tokens.length);
         return switch (cmd) {
             case "quit" -> "quit";
@@ -25,15 +35,37 @@ public class PreloginClient {
 
 
     //not implemented todavia
-    private String login(String[] params) {
-        return "";
+    private String login(String[] params)  {
+        if (params.length != 2) {
+            return "Expected: login <USERNAME> <PASSWORD>";
+        }
+        try {
+            LoginResult result = facade.login(params[0], params[1]);
+            this.authToken = result.authToken();
+            return "Logged in as " + result.username() + ".";
+        } catch (ResponseException e) {
+            return "Login failed. Check your username and password.";
+        }
     }
 
     private String register(String[] params) {
-        return "";
+        if (params.length != 3) {
+            return "Expected: register <USERNAME> <PASSWORD> <EMAIL>";
+        }
+        try {
+            RegisterResult result = facade.register(params[0], params[1], params[2]);
+            this.authToken = result.authToken();
+            return "Logged in as " + result.username() + ".";
+        } catch (ResponseException e) {
+            return "Could not register. That username may already be taken.";
+        }
     }
 
     private String help() {
-        return "";
+        return """
+            register <USERNAME> <PASSWORD> <EMAIL> - create an account
+            login <USERNAME> <PASSWORD> - log in to play
+            quit - exit the program
+            help - show this message""";
     }
 }
