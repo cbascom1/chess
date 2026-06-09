99a4c265c9039678a8e9928933ee7efb8d649b53
carson <carsonbascom@gmail.com>
2026-06-08 18:34:44 -0600

    fixed ServerMain.java

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -4,7 +4,12 @@ import chess.*;
 
 public class ServerMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        int port = 8080;
+        if (args.length == 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        Server server = new Server();
+        server.run(port);
+        System.out.println("Server is running on port " + port);
     }
 }
